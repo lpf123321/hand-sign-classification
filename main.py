767f16eafb55a9c107acd87a62b9aa532db25c1a
ysm <57093885+lpf123321@users.noreply.github.com>
2025-08-10 00:17:07 +0800

    Create main.py

---
+++ main.py
@@ -0,0 +1,105 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torch.utils.data import DataLoader
+from torchvision import datasets, transforms
+
+# 1. data preprocess
+train_transform = transforms.Compose([
+    transforms.Grayscale(num_output_channels=1), 
+    transforms.RandomRotation(10),               # randomly rotate ±10°
+    transforms.RandomAffine(degrees=0, translate=(0.1, 0.1), scale=(0.9, 1.1), shear=5),  
+    transforms.ColorJitter(brightness=0.2, contrast=0.2),  
+    transforms.ToTensor(),
+    transforms.Normalize((0.5,), (0.5,))
+])
+test_transform = transforms.Compose([
+    transforms.Grayscale(num_output_channels=1),
+    transforms.ToTensor(),
+    transforms.Normalize((0.5,), (0.5,))
+])
+# 2. load data
+train_data = datasets.ImageFolder(root='/kaggle/input/hand-sign-images/Train',\
+                                  transform=train_transform)
+test_data = datasets.ImageFolder(root='/kaggle/input/hand-sign-images/Test',\
+                                 transform=test_transform)
+
+train_loader = DataLoader(train_data, batch_size=64, shuffle=True)
+test_loader = DataLoader(test_data, batch_size=64, shuffle=False)
+
+# 3. define a simple CNN
+class SimpleCNN(nn.Module):
+    def __init__(self, num_classes=24):
+        super(SimpleCNN, self).__init__()
+        self.features = nn.Sequential(
+            nn.Conv2d(1, 32, kernel_size=3, padding=1),
+            nn.BatchNorm2d(32),
+            nn.ReLU(),
+            nn.MaxPool2d(2, 2),
+
+            nn.Conv2d(32, 64, kernel_size=3, padding=1),
+            nn.BatchNorm2d(64),
+            nn.ReLU(),
+            nn.MaxPool2d(2, 2),
+
+            nn.Conv2d(64, 128, kernel_size=3, padding=1),
+            nn.BatchNorm2d(128),
+            nn.ReLU(),
+            nn.MaxPool2d(2, 2)
+        )
+        self.classifier = nn.Sequential(
+            #nn.Dropout(0.5), 
+            nn.Linear(128 * 3 * 3, 256),
+            nn.ReLU(),
+            #nn.Dropout(0.5),
+            nn.Linear(256, 24)
+        )
+
+    def forward(self, x):
+        x = self.features(x)
+        x = x.view(x.size(0), -1)  # flatten
+        x = self.classifier(x)
+        return x
+        
+# 4. define loss function and optimizer
+device = torch.device("cuda")
+model = SimpleCNN(num_classes=24).to(device)
+criterion = nn.CrossEntropyLoss()
+optimizer = optim.Adam(model.parameters(), lr=0.01)
+
+# 5. train
+for epoch in range(10):
+    model.train()
+    running_loss = 0.0
+    correct_train, total_train = 0, 0
+    for images, labels in train_loader:
+        images, labels = images.to(device), labels.to(device)
+
+        optimizer.zero_grad()
+        outputs = model(images)
+        loss = criterion(outputs, labels)
+        loss.backward()
+        optimizer.step()
+        
+        running_loss += loss.item()
+        _, predicted = torch.max(outputs, 1)
+        total_train += labels.size(0)
+        correct_train += (predicted == labels).sum().item()
+        
+    train_acc = 100 * correct_train / total_train
+    print(f"Epoch {epoch+1} "
+    f"Loss: {running_loss/len(train_loader):.4f}, \
+    Train Accuracy: {train_acc:.2f}%")
+
+# 6. test
+model.eval()
+correct, total = 0, 0
+with torch.no_grad():
+    for images, labels in test_loader:
+        images, labels = images.to(device), labels.to(device)
+        outputs = model(images)
+        _, predicted = torch.max(outputs, 1)
+        total += labels.size(0)
+        correct += (predicted == labels).sum().item()
+
+print(f"Test Accuracy: {100 * correct / total:.2f}%")
